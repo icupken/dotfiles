3b937f1e91846a930ad7a811cad6c33b824e1063
Artem <59444437+icupken@users.noreply.github.com>
2021-12-05 15:01:50 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,13 +3,19 @@ echo "Install yay..."
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
+cd ..
+rm -rf yay
 echo "Yay installed."
 yay -S polybar ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen zscroll-git --noconfirm
 pacman -S acpi alsa-utils dunst fd nvidia bspwm feh picom rofi sxhkd firefox thunar xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
+git clone https://github.com/icupken/dotfiles.git
+cd dotfiles
 cp -r .config ~/
 cp  .Xresources ~/
 mkdir -p ~/.local/share/fonts
 cp -r fonts ~/.local/share/fonts
 cp .xinitrc ~/
 cp .bashrc ~/
+cd ..
+rm -rf dotfiles
 echo "Instalation end."
