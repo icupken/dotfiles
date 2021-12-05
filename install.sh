027c4dbc463a982966b3832cbf609a569b2223b4
Artem <59444437+icupken@users.noreply.github.com>
2021-12-05 15:17:59 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,14 +8,10 @@ rm -rf yay
 echo "Yay installed."
 yay -S polybar ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen zscroll-git --noconfirm
 pacman -S acpi alsa-utils dunst fd nvidia bspwm feh picom rofi sxhkd firefox thunar xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
-git clone https://github.com/icupken/dotfiles.git
-cd dotfiles
 cp -r .config ~/
 cp  .Xresources ~/
-mkdir -p ~/.local/share/fonts
-cp -r fonts ~/.local/share/fonts
+mkdir -p ~/.local/share/
+cp -r fonts ~/.local/share/
 cp .xinitrc ~/
 cp .bashrc ~/
-cd ..
-rm -rf dotfiles
 echo "Instalation end."
