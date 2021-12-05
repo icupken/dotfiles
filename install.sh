007a19af0fa2b970967b7821fc9e57b7eb20217d
Artem <59444437+icupken@users.noreply.github.com>
2021-12-05 16:32:56 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ..
 rm -rf yay
 echo "Yay installed."
 yay -S polybar ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen zscroll-git --noconfirm
-sudo pacman -S acpi alsa-utils dunst fd nvidia bspwm feh picom rofi sxhkd firefox thunar xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
+sudo pacman -S acpi alsa-utils dunst fd nvidia ntfs-3g gvfs bspwm feh picom rofi sxhkd firefox thunar xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
 cp -r .config ~/
 cp  .Xresources ~/
 mkdir -p ~/.local/share/
