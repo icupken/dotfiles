77800872113c541c33bd3abf3b50289003261c92
Artem <59444437+icupken@users.noreply.github.com>
2021-12-11 02:16:39 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,8 +6,18 @@ makepkg -si --noconfirm
 cd ..
 rm -rf yay
 echo "Yay installed."
-yay -S polybar ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen zscroll-git --noconfirm
-sudo pacman -S acpi alsa-utils dunst fd nvidia ntfs-3g gvfs bspwm feh picom rofi sxhkd firefox thunar xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
+
+# ------bspwm install----------
+# yay -S polybar ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen zscroll-git --noconfirm
+# sudo pacman -S acpi alsa-utils dunst fd nvidia ntfs-3g gvfs bspwm feh picom rofi sxhkd firefox thunar \
+#xorg maim xorg-xinit pulseaudio pavucontrol playerctl ttf-iosevka-nerd exa --noconfirm
+
+# ------xfce4 install----------
+sudo pacman -S alsa-utils fd nvidia ntfs-3g gvfs rofi sxhkd firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono \
+pavucontrol ttf-iosevka-nerd exa xfce4 xfce4-pulseaudio-plugin xfce4-weather-plugin --noconfirm
+
+yay -S ttf-material-design-icons-extended nerd-fonts-jetbrains-mono --noconfirm
+
 cp -r .config ~/
 cp  .Xresources ~/
 mkdir -p ~/.local/share/
