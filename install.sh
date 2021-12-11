dd88f9bd6c9fb5ec40290fa7b050d9a29cf9bfa3
Artem <59444437+icupken@users.noreply.github.com>
2021-12-11 21:42:36 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ..
 echo "Yay installed."
 
 # ------xfce4 install----------
-sudo pacman -S alsa-utils fd feh maim nvidia ntfs-3g gvfs rofi sxhkd firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono \
+sudo pacman -S alsa-utils fd feh maim nvidia ntfs-3g thunar discord gvfs rofi sxhkd firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono \
 pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst --noconfirm
 yay -S ttf-material-design-icons-extended nerd-fonts-jetbrains-mono deezer betterlockscreen --noconfirm
 
@@ -20,4 +20,5 @@ cp .bashrc ~/
 git clone https://github.com/siduck/st.git
 cd st
 sudo make install
+mkdir Screenshots ~/
 echo "Instalation end."
