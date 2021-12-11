366247186cd423a0639b947367066959b5fae900
Artem <59444437+icupken@users.noreply.github.com>
2021-12-11 22:02:20 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,10 +6,12 @@ makepkg -si --noconfirm
 cd ..
 echo "Yay installed."
 
-# ------xfce4 install----------
-sudo pacman -S alsa-utils fd feh maim nvidia ntfs-3g thunar discord gvfs rofi sxhkd firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono \
-pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst --noconfirm
-yay -S ttf-material-design-icons-extended nerd-fonts-jetbrains-mono deezer betterlockscreen --noconfirm
+# ------xfwm4 install----------
+sudo pacman -S alsa-utils fd feh maim nvidia ntfs-3g thunar gvfs \
+rofi sxhkd firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono \
+pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst okular mousepad --noconfirm
+
+yay -S ttf-material-design-icons-extended nerd-fonts-jetbrains-mono betterlockscreen --noconfirm
 
 cp -r .config ~/
 cp  .Xresources ~/
