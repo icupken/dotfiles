4fdb6ee8002f3511dcb2e7eec94d601e3f7127c0
Artem <59444437+icupken@users.noreply.github.com>
2022-01-22 23:00:08 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ cd ..
 echo "Yay installed."
 
 # ------xfwm4 install----------
-sudo pacman -S alsa-utils fd feh nvidia f2fs-tools ntfs-3g thunar gvfs \
+sudo pacman -S alsa-utils fd feh alacritty nvidia f2fs-tools ntfs-3g thunar gvfs \
 rofi firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono sxhkd hunspell-ru \
 pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst okular neovim flameshot playerctl --noconfirm
 
@@ -19,9 +19,6 @@ mkdir -p ~/.local/share/
 cp -r fonts ~/.local/share/
 cp .xinitrc ~/
 cp .bashrc ~/
-git clone https://github.com/siduck/st.git
-cd st
-sudo make install
 mkdir ~/Screenshots
 localectl --no-convert set-x11-keymap us,ru "" "" grp:alt_shift_toggle
 echo "Instalation end."
