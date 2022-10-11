aa9bec375acd0e6ec8bc60d02a3ce8cf39b84d44
Artem <59444437+icupken@users.noreply.github.com>
2022-10-11 22:22:18 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -7,9 +7,9 @@ cd ..
 echo "Yay installed."
 
 # ------xfwm4 install----------
-sudo pacman -S alsa-utils exa bat tint2 telegram-desktop gxkb fd feh alacritty nvidia f2fs-tools ntfs-3g gvfs \
+sudo pacman -S alsa-utils exa bat tint2 telegram-desktop gxkb fd feh nm-applet alacritty nvidia f2fs-tools ntfs-3g gvfs \
 firefox xorg xorg-xinit pulseaudio ttf-jetbrains-mono sxhkd hunspell-ru \
-pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst pcmanfm-qt okular helix flameshot --noconfirm
+pavucontrol ttf-iosevka-nerd exa xfwm4 picom dunst pcmanfm-qt oxygen-icons nitrogen okular helix flameshot --noconfirm
 
 yay -S polybar ttf-material-design-icons-extended ulauncher nerd-fonts-jetbrains-mono betterlockscreen --noconfirm
 
